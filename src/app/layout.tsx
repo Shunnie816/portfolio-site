259ab7f87b1074d5e0410fb2c01010d7cf971cb2
Shunnie816 <ponyo.shun0816@gmail.com>
2024-08-05 08:44:45 +0900

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Shun's Portfolio",
   description:
-    "This is Shun Yoshiya, a frontend developer. This page tells about myself and shows my skills.",
+    "This is Shun Yoshiya, a frontend developer. This is my portfolio site which tells about myself and shows my skills.",
 };
 
 export default function RootLayout({
